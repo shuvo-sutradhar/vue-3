ed284238adc30aa994bb3d3116a92bfe536f8eb9
shuvo-sutradhar <shuvossd1@gmail.com>
2021-10-09 16:12:35 +0600

    1st teleport

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <Counter/>
-  <CustomRef/>
+  <ComponentA msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import CustomRef from './components/CustomRef.vue'
-import Counter from './components/Counter.vue'
+import ComponentA from './components/ComponentA.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Counter,
-    CustomRef,
+    ComponentA
   }
 }
 </script>
